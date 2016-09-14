3351bff9a19049bc588fd9b068a6ebcfc0d719d7
ESchulstad <eschulstad@gmail.com>
2016-09-13 20:40:56 -0400

    Initial Commit

---
+++ LongestWord/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LongestWord
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            string[] items = input.Split(' ');
+
+            string longWord = "";
+
+            for(int i =0;i<=items.Length-1;i++)
+            {
+                if(items[i].Length > longWord.Length)
+                {
+                    longWord = items[i];
+                }
+                
+            }
+            System.Console.WriteLine(longWord);
+
+        }
+    }
+}
